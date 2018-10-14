78487e7694025d9e8205070bf7cf3f217609751c
Seia <30369714+Seia-Soto@users.noreply.github.com>
2018-10-14 23:38:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const base = {
   userAgent: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 }
 
-const build = (options) =>{
+const build = (options) => {
   const built = {
     context: options.content || '',
     from: options.from || 'auto',
